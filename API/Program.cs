1dd423b0a41d458f834499f8d04705bca6a7a46a
Tanaka <tnk3atan@gmail.com>
2025-06-04 11:09:17 +0700

    voucher_service

--- API/Program.cs
+++ API/Program.cs
@@ -1,30 +1,28 @@
-<<<<<<< HEAD
+﻿using API.IRepository;
 using API.IRepository.Repository;
-using API.IRepository;
-=======
 using API.Repository;
 using API.Repository.IRepository;
->>>>>>> 57f4e0e97e8167b8074d442aed1855af67d265df
 using Data.Models;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<DbContextApp>(x => x.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
-builder.Services.AddScoped<IGiayChiTietRepository, GiayChiTietRepository>();
-builder.Services.AddScoped<IDeGiayRepository , DeGiayRepository>();
 
+// Đăng ký DbContext (chỉ 1 lần duy nhất)
 builder.Services.AddDbContext<DbContextApp>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+
+// Đăng ký các Repository
+builder.Services.AddScoped<IGiayChiTietRepository, GiayChiTietRepository>();
+builder.Services.AddScoped<IDeGiayRepository, DeGiayRepository>();
 builder.Services.AddScoped<INhanVienRepository, NhanVienRepository>();
+builder.Services.AddScoped<IVoucherRepo, VoucherRepo>();
 
 var app = builder.Build();
 
