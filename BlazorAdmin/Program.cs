7d90eafece44a0b97d1fe423360af73281268676
TruongLeo <truongnnph50099@gmail.com>
2025-06-05 14:10:37 +0700

    ok

--- BlazorAdmin/Program.cs
+++ BlazorAdmin/Program.cs
@@ -1,4 +1,6 @@
-﻿using BlazorAdmin.Components;
+using API.IService;
+using API.Service;
+using BlazorAdmin.Components;
 using BlazorAdmin.Service;
 using BlazorAdmin.Service.IService;
 
@@ -8,37 +10,34 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+
 builder.Services.AddScoped(sp => new HttpClient
 {
-    BaseAddress = new Uri("https://localhost:7246/")
+    BaseAddress = new Uri("https://localhost:7246/") 
 });
-
-// Đăng ký HTTP Clients
 builder.Services.AddHttpClient("voucher", client =>
 {
     client.BaseAddress = new Uri("https://localhost:7246/");
 });
-
-builder.Services.AddHttpClient("hoadon", client =>
-{
-    client.BaseAddress = new Uri("https://localhost:7246/");
-});
-
-// Đăng ký Service
 builder.Services.AddScoped<INhanVienService, NhanVienService>();
-builder.Services.AddScoped<IVoucherService, VoucherServiceRepo>();
-builder.Services.AddScoped<IHoaDonService, HoaDonServiceRepo>(); 
-
+builder.Services.AddScoped<IAnhService, AnhService>();
+builder.Services.AddScoped<IChatLieuService, ChatLieuService>();
+builder.Services.AddScoped<IGiayService, GiayService>();
+builder.Services.AddScoped<IHoaDonService, HoaDonService>();
+builder.Services.AddScoped<IThuongHieuService, ThuongHieuService>();
+builder.Services.AddScoped<IVoucherService, VoucherService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
+
 app.UseStaticFiles();
 app.UseAntiforgery();
 
