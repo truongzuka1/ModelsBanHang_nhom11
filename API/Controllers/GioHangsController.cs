170b2f9b0b294003a61693c20616405a0c26aaff
OnlyK <khai5108@gmail.com>
2025-06-05 11:10:23 +0700

    UpdateGioHang

--- API/Controllers/GioHangsController.cs
+++ API/Controllers/GioHangsController.cs
@@ -6,159 +6,58 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Data.Models;
+using API.IRepository;
 
 namespace API.Controllers
 {
     public class GioHangsController : Controller
     {
-        private readonly DbContextApp _context;
+        private readonly IGioHangRepository _giohang;
 
-        public GioHangsController(DbContextApp context)
+        public GioHangsController(IGioHangRepository giohang)
         {
-            _context = context;
+            _giohang = giohang;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAllGioHang()
+        {
+            return Ok(await _giohang.GetAllGioHang());
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetGioHangByID(Guid id) {
+            return Ok(await _giohang.GetGioHang(id));
         }
 
-        // GET: GioHangs
-        public async Task<IActionResult> Index()
-        {
-            var dbContextApp = _context.GioHangs.Include(g => g.KhachHang);
-            return View(await dbContextApp.ToListAsync());
-        }
 
-        // GET: GioHangs/Details/5
-        public async Task<IActionResult> Details(Guid? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var gioHang = await _context.GioHangs
-                .Include(g => g.KhachHang)
-                .FirstOrDefaultAsync(m => m.GioHangId == id);
-            if (gioHang == null)
-            {
-                return NotFound();
-            }
-
-            return View(gioHang);
-        }
-
-        // GET: GioHangs/Create
-        public IActionResult Create()
-        {
-            ViewData["KhachHangId"] = new SelectList(_context.KhachHangs, "KhachHangId", "Email");
-            return View();
-        }
-
-        // POST: GioHangs/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("GioHangId,NgayTaoGioHang,NgayCapNhatCuoiCung,TrangThai,KhachHangId")] GioHang gioHang)
+        public async Task<IActionResult> Create( GioHang gioHang)
         {
-            if (ModelState.IsValid)
-            {
-                gioHang.GioHangId = Guid.NewGuid();
-                _context.Add(gioHang);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["KhachHangId"] = new SelectList(_context.KhachHangs, "KhachHangId", "Email", gioHang.KhachHangId);
-            return View(gioHang);
+            await _giohang.CreateGioHang(gioHang);
+            return Ok();
         }
 
-        // GET: GioHangs/Edit/5
-        public async Task<IActionResult> Edit(Guid? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
 
-            var gioHang = await _context.GioHangs.FindAsync(id);
-            if (gioHang == null)
-            {
-                return NotFound();
-            }
-            ViewData["KhachHangId"] = new SelectList(_context.KhachHangs, "KhachHangId", "Email", gioHang.KhachHangId);
-            return View(gioHang);
-        }
 
-        // POST: GioHangs/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(Guid id, [Bind("GioHangId,NgayTaoGioHang,NgayCapNhatCuoiCung,TrangThai,KhachHangId")] GioHang gioHang)
+        public async Task<IActionResult> Edit( GioHang gioHang)
         {
-            if (id != gioHang.GioHangId)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(gioHang);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!GioHangExists(gioHang.GioHangId))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["KhachHangId"] = new SelectList(_context.KhachHangs, "KhachHangId", "Email", gioHang.KhachHangId);
-            return View(gioHang);
+            await _giohang.UpdateGioHang(gioHang);
+            return Ok();
+            
         }
 
-        // GET: GioHangs/Delete/5
-        public async Task<IActionResult> Delete(Guid? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
 
-            var gioHang = await _context.GioHangs
-                .Include(g => g.KhachHang)
-                .FirstOrDefaultAsync(m => m.GioHangId == id);
-            if (gioHang == null)
-            {
-                return NotFound();
-            }
 
-            return View(gioHang);
-        }
-
-        // POST: GioHangs/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(Guid id)
         {
-            var gioHang = await _context.GioHangs.FindAsync(id);
-            if (gioHang != null)
-            {
-                _context.GioHangs.Remove(gioHang);
-            }
-
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
+            await _giohang.DeleteGioHang(id);
+            return Ok();
         }
 
-        private bool GioHangExists(Guid id)
-        {
-            return _context.GioHangs.Any(e => e.GioHangId == id);
-        }
     }
 }
