b65129f926bec083abe6f1df658eca25c119a631
truongnnph50099@gmail.com <truongnnph50099@gmail.com>
2025-07-16 13:52:43 +0700

    update voucher

--- API/IRepository/IGioHangRepository.cs
+++ API/IRepository/IGioHangRepository.cs
@@ -3,18 +3,12 @@ using Data.Models;
 
 namespace API.IRepository
 {
-    //public interface IGioHangRepository
-    //{
-    //    Task<IEnumerable<GioHang>> GetAllGioHang();
-    //    Task<GioHang> GetGioHang(Guid id);
-    //    Task CreateGioHang(GioHang gh);
-    //    Task UpdateGioHang(GioHang gh);
-    //    Task DeleteGioHang(Guid id);
-    //}
     public interface IGioHangRepository
     {
-        Task<GioHang> GetByTaiKhoanIdAsync(Guid taiKhoanId);
-        Task<bool> CapNhatSoLuongAsync(Guid chiTietId, int soLuong);
-        Task<bool> XoaSanPhamAsync(Guid chiTietId);
+        Task<IEnumerable<GioHang>> GetAllGioHang();
+        Task<GioHang> GetGioHang(Guid id);
+        Task CreateGioHang(GioHang gh);
+        Task UpdateGioHang(GioHang gh);
+        Task DeleteGioHang(Guid id);
     }
 }
