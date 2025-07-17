e30db349a81ebdf2bcc3fd3029b71d57f36e28c2
OnlyK <khai5108@gmail.com>
2025-07-17 15:55:07 +0700

    update

--- API/Models/ThongBao.cs
+++ API/Models/ThongBao.cs
@@ -5,6 +5,8 @@ namespace API.Models
     public class ThongBao
     {
         [Key]
+
+
         public Guid Id { get; set; } = Guid.NewGuid();
         public string NoiDung { get; set; } = string.Empty;
         public DateTime ThoiGian { get; set; } = DateTime.Now;
